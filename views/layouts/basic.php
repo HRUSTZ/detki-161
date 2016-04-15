6119ffa59d5cf429a49ced79be3de882bc9deaab
HRUSTZ <hawk_vladimir@inbox.ru>
2016-04-15 18:45:44 +0300

    first commit

---
+++ views/layouts/basic.php
@@ -0,0 +1,16 @@
+<?php
+/* @var $content string*/
+?>
+<!DOCTYRE html>
+<html>
+<head>
+    <title></title>
+</head>
+<body>
+<p>Верхняя часть сайта<p>
+
+<?= $content ?>
+
+<p>Нижняя часть сайта<p>
+</body>
+</html>
